d8347d88303501d89336475059d17d7a2bfea907
Your Name <berteng90@gmail.com>
2023-02-05 05:33:00 +0800

    Update Logic

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-const searchbar = document.querySelector(".searchbar");
+(() => {
+  const searchbar = document.querySelector(".searchbar");
+  searchbar.addEventListener("input", () => {
+    const cityName = searchbar.value;
+    fetchWeatherDataLocation(cityName);
+  });
+})();
 
-const fetchWeatherData = async (location) => {
+const fetchWeatherDataLocation = async (location) => {
   const loc = location;
   const response = await fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${loc}&limit=10&appid=87eefaf1dae2bd0cc45a46101dfe1f24`,
@@ -11,12 +17,42 @@ const fetchWeatherData = async (location) => {
   )
     .then((data) => data.json())
     .then((weatherData) => {
+      const ul = document.querySelector("#searchbar-results");
+      ul.innerHTML = "";
       for (i = 0; i < weatherData.length; i++) {
+        const li = document.createElement("li");
+        const locList = {
+          name: weatherData[i].name,
+          country: weatherData[i].country,
+          state: weatherData[i].state,
+          lat: weatherData[i].lat,
+          lon: weatherData[i].lon,
+        };
+        li.textContent = `${locList.name}, ${locList.state} (${locList.country})`;
+        ul.appendChild(li);
         console.log(weatherData[i]);
+        li.addEventListener("click", () => {
+          const searchbar = document.querySelector(".searchbar");
+          ul.innerHTML = "";
+          searchbar.value = li.textContent;
+          fetchWeatherData(locList.lat, locList.lon);
+        });
       }
     });
 };
-fetchWeatherData("Don Carlos");
+
+const fetchWeatherData = async (lat, lon) => {
+  console.log(lat, lon);
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=87eefaf1dae2bd0cc45a46101dfe1f24`,
+    {
+      mode: "cors",
+      method: "GET",
+    }
+  )
+    .then((data) => data.json())
+    .then((weatherData) => console.log(weatherData));
+};
 
 // OPEN WEATHER GEOLOCATION API
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
